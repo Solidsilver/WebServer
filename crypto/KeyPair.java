0ada2a3e1860ea75ef6c09c59d525cd00e09cef3
Solidsilver <lukemattfeld@gmail.com>
2019-05-13 19:39:06 -0700

    oopsie in not renaming symbol

--- crypto/KeyPair.java
+++ crypto/KeyPair.java
@@ -22,11 +22,11 @@ public class KeyPair implements Serializable {
         return this.privKey;
     }
 
-    public static KeyPair generateKeyPair(int keyLength) {
+    public static KeyPair generateKeyPair(int keySize) {
         Key privKey = null;
         Key pubKey = null;
-        BigInteger p = BigInteger.probablePrime((int)primeSize/2, new Random());
-        BigInteger q = BigInteger.probablePrime((int)primeSize/2, new Random());
+        BigInteger p = BigInteger.probablePrime((int)keySize/2, new Random());
+        BigInteger q = BigInteger.probablePrime((int)keySize/2, new Random());
         BigInteger n = p.multiply(q);
         BigInteger ph = lcm(p.subtract(BigInteger.ONE), q.subtract(BigInteger.ONE));
         BigInteger e;
